ee4338a8b75e98e47e841312ec1bb67ff8235e1d
Alvin Coulson <aaacoulson@gmail.com>
2020-04-10 22:56:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,10 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costumestores do |t|
       t.string :name
       t.string :location
-      
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.string :still_in_business
+      t.datetime :opening_time
     end
   end
 end
