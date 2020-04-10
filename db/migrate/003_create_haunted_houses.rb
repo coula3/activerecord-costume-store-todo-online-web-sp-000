27e4c5f55106934b32d01856acd03680e537ae30
Alvin Coulson <aaacoulson@gmail.com>
2020-04-10 23:13:14 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouse < ActiveRecord::Migration
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text :long_description
+      t.text :description
     end
   end
 end
