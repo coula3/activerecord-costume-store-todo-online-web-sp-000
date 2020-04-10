e1785d061a7f829160f6fe255084a79a2e4ae36c
Alvin Coulson <aaacoulson@gmail.com>
2020-04-10 23:09:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,10 @@
 class CreateHauntedHouse < ActiveRecord::Migration
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
     end
   end
 end
