7198b1b8010a70f7f6a2f62c243564ca6c806887
Alvin Coulson <aaacoulson@gmail.com>
2020-04-10 22:47:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,9 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table costumes do |t|
       t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
       
     end
   end
